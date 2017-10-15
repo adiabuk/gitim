15a6c82171f215f5e132d9876a720a428fb2db42
Amro Diab <adiab@hotmail.co.uk>
2017-10-15 14:33:21 +0100

    cleanup & lint

--- setup.py
+++ setup.py
@@ -1,26 +1,24 @@
-from setuptools import setup, find_packages
-try:
-    from itertools import imap, ifilter
-except ImportError:
-    imap = map
-    ifilter = filter
-from os import path
-from ast import parse
+#!/usr/bin/env python
+
+"""
+Setup script for gitim
+"""
+
+from setuptools import setup
+from gitim import (__version__ as VERSION, __author__ as AUTHOR,
+                   __name__ as NAME, __license__ as LICENSE)
 
 if __name__ == '__main__':
-    package_name = 'gitim'
 
-    get_vals = lambda var0, var1: imap(lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-                                       ifilter(lambda line: line.startswith(var0) or line.startswith(var1), f))
-
-    with open('gitim.py') as f:
-        __author__, __version__ = get_vals('__version__', '__author__')
+    with open('requirements.txt') as reqs_file:
+        REQS = reqs_file.readlines()
 
     setup(
-        name=package_name,
-        author=__author__,
-        version=__version__,
-        license='MIT',
-        install_requires=['pygithub'],
-        py_modules=['gitim']
+        name=NAME,
+        author=AUTHOR,
+        version=VERSION,
+        license=LICENSE,
+        install_requires=REQS,
+        entry_points={'console_scripts':['gitim=gitim:main']},
+        py_modules=[NAME]
     )
